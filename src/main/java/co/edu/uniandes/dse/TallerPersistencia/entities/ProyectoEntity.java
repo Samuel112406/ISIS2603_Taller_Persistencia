60f8ec04cf8c90d0b8ead43a2e0ef894adade3f0
Hijark <s.castroj@uniandes.edu.co>
2026-02-05 10:37:56 -0500

    minimo cambio FINAL

--- src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/ProyectoEntity.java
+++ src/main/java/co/edu/uniandes/dse/TallerPersistencia/entities/ProyectoEntity.java
@@ -2,12 +2,13 @@ package co.edu.uniandes.dse.TallerPersistencia.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class ProyectoEntity {
     
    @OneToMany(mappedBy = "proyecto", orphanRemoval = true)
-   private TareaEntity tarea;
+   private List<TareaEntity> tarea;
 
    private String nombre;
    private String descripcion;
